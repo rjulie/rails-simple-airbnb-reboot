278210f8e027feaf0d90a9c1fd85b48a0c197d3b
Julie Reynal <rjulie.pro@gmail.com>
2022-12-22 11:39:50 +0100

    views index and show

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :description, presence: true
+  validates :price_per_night, :number_of_guests, presence: true, numericality: { only_integer: true }
 end
