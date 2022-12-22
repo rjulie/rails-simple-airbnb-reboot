f977b0e68116fc29552acdb4e0ce4975ed52457f
Julie Reynal <rjulie.pro@gmail.com>
2022-12-22 14:44:05 +0100

    search query

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,7 +3,14 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: %i[show edit update destroy]
 
   def index
-    @flats = Flat.all
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @flats = Flat.all
+    end
   end
 
   def show
